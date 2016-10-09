44c4700fad63536b691b91e1ab27fd0bb76400c3
MikhailNeizvestny <brainus05@yandex.ru>
2016-10-09 20:10:23 +0400

    Added entry point

--- RPIS41/Neizvestny/wdad/Application.java
+++ RPIS41/Neizvestny/wdad/Application.java
@@ -12,6 +12,6 @@ package RPIS41.Neizvestny.wdad;
 public class Application {
     public static void main(String[] args)
     {
-        System.out.println("I'm Neizvestny Mikhail, and I'm not a monkey");
+        System.out.println("I'm Neizvestny Mikhail, and I'm not a monkey!");
     }
 }
