c7b0050db3718aa232ad826a9a913cc7c95e460a
MikhailNeizvestny <brainus05@yandex.ru>
2016-10-30 18:14:27 +0400

    add .dtd and .xml file

---
+++ RPIS41/Neizvestny/wdad/learn/xml/XmlTask.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RPIS41.Neizvestny.wdad.learn.xml;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.*;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
+import org.w3c.dom.ls.LSSerializer;
+import org.xml.sax.SAXException;
+/**
+ *
+ * @author Cyfralus
+ */
+public class XmlTask {
+    public static void main(String[] args)
+    {
+    }
+    /*public double getBill(String street, int buildingNumber, int flatNumber)
+    {
+        
+    }
+    
+    public void setTariff(String tariffName, double newValue) 
+    {
+        
+    }
+    
+    public void addRegistration(String street, int buildingNumber, int flatNumber,
+            int year, int month, double coldWater, double hotWater, 
+            double electricity, double gas)
+    {
+        
+    }*/
+
+}
